f946bd12c7f257b7ccd52a21cf90239d2b945b22
Vincent Lin <linv@mit.edu>
2023-05-25 14:41:01 -0500

    Attempt at subtasking, wip

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,8 +1,6 @@
-import { Fragment } from "react"
-
-export default function TaskItem({ task }) {
+export default function TaskItem({ task, handleSelectTask }) {
     return (
-        <li className="taskItem">
+        <li className="taskItem" onClick={(e) => handleSelectTask(task.id)}>
             <input className="taskItemCheckbox" type="checkbox" />
             <div className="taskItemText">
                 <p className="taskTitle">{task.title}</p>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,15 +3,21 @@ import TaskInput from "./TaskInput";
 import TaskItem from "./TaskItem";
 import "../App.css"
 
-function Task(title, description) {
+function Task(id, title, description, subNum, parent=null) {
+	this.id = id
     this.title = title;
     this.description = description;
+	this.subNum = subNum
+	this.parent = parent;
 }
 
 export default function TaskList() {
+	document.addEventListener("keydown", handleKeyDown);
+
     const [taskList, setTaskList] = useState([]);
 	const [idCounter, setIdCounter] = useState(0);
-	const [newTask, setNewTask] = useState({id: idCounter, title: "", description: ""});
+	const [newTask, setNewTask] = useState(new Task(idCounter, "", "", 0));
+	const [selectedTaskId, setSelectedTaskId] = useState(null);
 
 	function handleNewTaskChange(e) {
 		e.preventDefault();
@@ -27,8 +33,40 @@ export default function TaskList() {
 			return;
 		}
 		setTaskList([newTask].concat(taskList))
-		setNewTask({id: idCounter + 1, title: "", description: ""});
+		setNewTask(new Task(idCounter + 1, "", "", 0));
 		setIdCounter(idCounter + 1);
+	}
+
+	function handleSelectTask(taskId) {
+		console.log(taskId)
+		setSelectedTaskId(taskId);
+	}
+
+	function handleKeyDown(e) {
+		console.log('keydown', selectedTaskId)
+		if (e.key === "ArrowRight" && (selectedTaskId !== null)) {
+			console.log('selected', selectedTaskId)
+			convertToSubtask(selectedTaskId)
+		}
+	}
+
+	function convertToSubtask(taskId) {
+		console.log('subtask', taskId)
+		const taskIx = taskList.findIndex((task) => task.id === taskId)
+		const task = taskList[taskIx];
+		if (taskIx > 0) { // Cannot make subtask of first task in list
+			// Search backwards from taskIx for the first "parent" task that has an equal subNum
+			const parentTask = [...taskList.slice(0, taskIx)].reverse().find(
+				(pt) => pt.subNum === task.subNum
+			)
+			// Create new subtask
+			const newSubtask = {
+				...task,
+				subNum: task.subNum + 1,
+				parent: parentTask
+			}
+			setTaskList(taskList.slice(taskIx).concat(newSubtask).concat(taskList.slice(taskIx + 1)));
+		}
 	}
 
     return (
@@ -41,7 +79,8 @@ export default function TaskList() {
                 {taskList.map((task) => (
 					<Fragment key={task.id}>
 						<TaskItem 
-							task={task} />
+							task={task}
+							handleSelectTask={handleSelectTask} />
 					</Fragment>))}
             </ul>
         </div>
